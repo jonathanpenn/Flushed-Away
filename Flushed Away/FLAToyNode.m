3938b71c02bc2a6b132ba3fa8078a6116034aa51
Michele <michele.titolo@gmail.com>
2013-09-10 20:33:39 -0600

    Added to masses

--- Flushed Away/FLAToyNode.m
+++ Flushed Away/FLAToyNode.m
@@ -10,53 +10,54 @@
 
 @implementation FLAToyNode
 
-+ (instancetype)toyNodeWithImageNamed:(NSString*)image
++ (instancetype)toyNodeWithImageNamed:(NSString*)image mass:(CGFloat)mass
 {
     FLAToyNode* node = [FLAToyNode spriteNodeWithImageNamed:image];
     node.size = node.texture.size;
     node.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:node.texture.size.width];
+    node.physicsBody.mass = mass;
     
     return node;
 }
 
 + (instancetype)bearToyNode
 {
-    return [self toyNodeWithImageNamed:@"toy-bear"];
+    return [self toyNodeWithImageNamed:@"toy-bear" mass:65];
 }
 
 + (instancetype)bikeToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-bike"];
+     return [self toyNodeWithImageNamed:@"toy-bike" mass:89];
 }
 
 + (instancetype)carToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-car"];
+     return [self toyNodeWithImageNamed:@"toy-car" mass:95];
 }
 
 + (instancetype)duckToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-duck"];
+     return [self toyNodeWithImageNamed:@"toy-duck" mass:50];
 }
 
 + (instancetype)horseToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-horse"];
+     return [self toyNodeWithImageNamed:@"toy-horse" mass:93];
 }
 
 + (instancetype)planeToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-plane"];
+     return [self toyNodeWithImageNamed:@"toy-plane" mass:99];
 }
 
 + (instancetype)scooterToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-scooter"];
+     return [self toyNodeWithImageNamed:@"toy-scooter" mass:78];
 }
 
 + (instancetype)soldierToyNode
 {
-     return [self toyNodeWithImageNamed:@"toy-soldier"];
+     return [self toyNodeWithImageNamed:@"toy-soldier" mass:52];
 }
 
 @end
