dff470bd1bf2cb4e32d8a4ce4477c1a44c8a8dee
Jonathan Penn <jonathan@navel-labs.com>
2013-09-11 09:30:37 -0600

    Added the credits

--- Flushed Away/FLAStartCreditsScene.m
+++ Flushed Away/FLAStartCreditsScene.m
@@ -23,21 +23,35 @@
         self.anchorPoint = CGPointMake(0.5, 0.5);
 
         SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"Copperplate"];
-        label.fontSize = 26;
+        label.fontSize = 30;
         label.fontColor = [SKColor whiteColor];
         label.text = @"Flushed Away";
         label.position = CGPointMake(0, 0);
         label.alpha = 1;
         [self addChild:label];
 
+        SKLabelNode *byLine = [SKLabelNode labelNodeWithFontNamed:@"Copperplate"];
+        byLine.fontSize = 20;
+        byLine.fontColor = [SKColor whiteColor];
+        byLine.text = @"By Jonathan Penn and Michele Titolo";
+        byLine.position = CGPointMake(0, -30);
+        byLine.alpha = 0;
+        [self addChild:byLine];
+
         [self runAction:[SKAction sequence:@[
-                                             [SKAction waitForDuration:6.0],
+                                             [SKAction waitForDuration:7.0],
                                              [SKAction runBlock:^{ [self goToNextScene]; }]
                                              ]]];
         [label runAction:[SKAction sequence:@[
-                                             [SKAction waitForDuration:4.0],
+                                             [SKAction waitForDuration:5.0],
                                              [SKAction fadeAlphaTo:0 duration:1],
                                              ]]];
+        [byLine runAction:[SKAction sequence:@[
+                                               [SKAction waitForDuration:2],
+                                               [SKAction fadeAlphaTo:1 duration:2],
+                                               [SKAction waitForDuration:1],
+                                               [SKAction fadeAlphaTo:0 duration:1]
+                                               ]]];
         [[FLASoundQueue sharedSoundQueue] queueSoundFileNamed:@"intro_music" loop:NO];
         [[FLASoundQueue sharedSoundQueue] start];
     }
